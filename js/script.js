b6ce6e513c29c3198eb7fde6246429e30ede66bf
salmaadindaa <salma.adindanisa@gmail.com>
2024-07-28 18:02:25 +0700

    membuat website kedai kopi sunrise boulevard

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Toggle class active . yg tdinya gaada jadi ada dan sebaliknya
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luar side bar utk menghilangkan nav nya
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+    navbarNav.classList.remove("active");
+});
